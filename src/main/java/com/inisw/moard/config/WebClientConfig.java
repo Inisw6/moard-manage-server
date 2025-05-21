02754a558d8f9ddcd9afa6a6efc0736769bbe622
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-21 17:22:09 +0900

    feat: web client config 추가

---
+++ src/main/java/com/inisw/moard/config/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.inisw.moard.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient naverWebClient(@Value("${naver.client.id}") String clientId,
+                                    @Value("${naver.client.secret}") String clientSecret) {
+        return WebClient.builder()
+                .baseUrl("https://openapi.naver.com")
+                .defaultHeader("X-Naver-Client-Id", clientId)
+                .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                .build();
+    }
+}
