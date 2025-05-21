a237cf9b769d1360574adf8ba8e6edfa520decd8
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-21 14:26:22 +0900

    chore: 패키지명 변경

--- src/main/java/com/inisw/moard/MoardApplication.java
+++ src/main/java/com/inisw/moard/MoardApplication.java
@@ -1,4 +1,4 @@
-package com.inisw;
+package com.inisw.moard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoardApplication.class, args);
+    }
 
 }
