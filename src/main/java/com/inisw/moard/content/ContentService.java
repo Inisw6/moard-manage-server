94dd2b6f44b33d07db2904541885fd64b02a177c
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-21 15:45:54 +0900

    feat: Content 서비스 추가

--- src/main/java/com/inisw/moard/content/ContentService.java
+++ src/main/java/com/inisw/moard/content/ContentService.java
@@ -1,4 +1,20 @@
 package com.inisw.moard.content;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ContentService {
+    private final ContentRepository contentRepository;
+
+    public Content createContent(Content content) {
+        return contentRepository.save(content);
+    }
+
+    public List<Content> createContentList(List<Content> contents) {
+        return contentRepository.saveAll(contents);
+    }
 }
