3ecbcd2418608b02b7e2c39ea6a4233774e280f4
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-21 17:52:22 +0900

    feat: Youtube Bean 추가

--- src/main/java/com/inisw/moard/config/WebClientConfig.java
+++ src/main/java/com/inisw/moard/config/WebClientConfig.java
@@ -3,6 +3,8 @@ package com.inisw.moard.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -14,6 +16,14 @@ public class WebClientConfig {
                 .baseUrl("https://openapi.naver.com")
                 .defaultHeader("X-Naver-Client-Id", clientId)
                 .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                .build();
+    }
+
+    @Bean(name = "youtubeWebClient")
+    public WebClient youtubeWebClient(@Value("${youtube.api.key}") String apiKey) {
+        return WebClient.builder()
+                .baseUrl("https://www.googleapis.com/youtube/v3")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
