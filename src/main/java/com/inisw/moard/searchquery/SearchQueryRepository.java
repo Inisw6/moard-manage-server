d18cb1f56474df07174c8114321396eca7528999
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-21 15:27:43 +0900

    feat: repository 코드 작성

--- src/main/java/com/inisw/moard/searchquery/SearchQueryRepository.java
+++ src/main/java/com/inisw/moard/searchquery/SearchQueryRepository.java
@@ -1,4 +1,9 @@
 package com.inisw.moard.searchquery;
 
-public interface SearchQueryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SearchQueryRepository extends JpaRepository<SearchQuery, Long> {
+    Optional<SearchQuery> findByQuery(String query);
 }
