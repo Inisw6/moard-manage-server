bf4fa9cd6aaf768e630e5c96b79d9271ab1d38ae
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-21 12:25:08 +0900

    initial

---
+++ src/main/java/com/inisw/MoardApplication.java
@@ -0,0 +1,13 @@
+package com.inisw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoardApplication.class, args);
+	}
+
+}
