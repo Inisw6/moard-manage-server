6c00adbe44282c63aa008f31c5e9ac7d560d7d02
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-26 14:16:09 +0900

    chore: 폴더 구조 수정

--- src/main/java/com/inisw/moard/content/ContentService.java
+++ src/main/java/com/inisw/moard/content/ContentService.java
@@ -1,44 +1,46 @@
 package com.inisw.moard.content;
 
-import com.inisw.moard.api.ContentAggregatorService;
-import com.inisw.moard.searchquery.SearchQuery;
-import com.inisw.moard.searchquery.SearchQueryRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.inisw.moard.api.ContentAggregatorService;
+import com.inisw.moard.content.searchquery.SearchQuery;
+import com.inisw.moard.content.searchquery.SearchQueryRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ContentService {
-    private final ContentRepository contentRepository;
-    private final SearchQueryRepository searchQueryRepository;
-    private final ContentAggregatorService contentAggregatorService;
+	private final ContentRepository contentRepository;
+	private final SearchQueryRepository searchQueryRepository;
+	private final ContentAggregatorService contentAggregatorService;
 
-    public Content createContent(Content content) {
-        return contentRepository.save(content);
-    }
+	public Content createContent(Content content) {
+		return contentRepository.save(content);
+	}
 
-    public List<Content> createContentList(List<Content> contents) {
-        return contentRepository.saveAll(contents);
-    }
+	public List<Content> createContentList(List<Content> contents) {
+		return contentRepository.saveAll(contents);
+	}
 
-    public List<Content> readContentsByQuery(String query, Integer maxResults) {
-        List<Content> result;
+	public List<Content> readContentsByQuery(String query, Integer maxResults) {
+		List<Content> result;
 
-        SearchQuery searchQuery = searchQueryRepository.findByQuery(query).orElse(null);
-        if (searchQuery != null) {
-            result = searchQuery.getContentList();
-        } else {
-            List<Content> contents = contentAggregatorService.aggregate(query, maxResults);
+		SearchQuery searchQuery = searchQueryRepository.findByQuery(query).orElse(null);
+		if (searchQuery != null) {
+			result = searchQuery.getContentList();
+		} else {
+			List<Content> contents = contentAggregatorService.aggregate(query, maxResults);
 
-            searchQuery = SearchQuery.builder()
-                    .query(query)
-                    .contentList(contents)
-                    .build();
-            SearchQuery savedSearchQuery = searchQueryRepository.save(searchQuery);
-            result = searchQuery.getContentList();
-        }
-        return result;
-    }
+			searchQuery = SearchQuery.builder()
+				.query(query)
+				.contentList(contents)
+				.build();
+			SearchQuery savedSearchQuery = searchQueryRepository.save(searchQuery);
+			result = searchQuery.getContentList();
+		}
+		return result;
+	}
 }
