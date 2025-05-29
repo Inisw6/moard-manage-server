6500dd1b87c3a1134afbc29188c660313ebfeb5b
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-29 14:54:52 +0900

    feat: 쿼리에 따른 저장 부분 수정

--- src/main/java/com/inisw/moard/stock/StockInfo.java
+++ src/main/java/com/inisw/moard/stock/StockInfo.java
@@ -1,9 +1,17 @@
 package com.inisw.moard.stock;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.inisw.moard.content.searchquery.SearchQuery;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,4 +35,9 @@ public class StockInfo {
 	private String industryType;
 	private String marketType;
 	private String industryDetail;
+
+	@OneToMany(mappedBy = "stockInfo", cascade = CascadeType.ALL, orphanRemoval = true)
+	@Builder.Default
+	@JsonManagedReference
+	private List<SearchQuery> searchQueryList = new ArrayList<>();
 }
--- src/main/java/com/inisw/moard/stock/StockInfoRepository.java
+++ src/main/java/com/inisw/moard/stock/StockInfoRepository.java
@@ -1,6 +1,9 @@
 package com.inisw.moard.stock;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StockInfoRepository extends JpaRepository<StockInfo, Long> {
+	Optional<StockInfo> findByName(String query);
 }
